5550db944a878e6f17ff997896b07e390df46a81
dola <mohamed.adeltawfiq@roaa.tech>
2022-12-25 18:21:59 -0800

    add todo list controller

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+let item = ["dola","mohamed","ahmed","abdo"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = item[indexPath.row]
+        
+        return cell
+    }
+}
+
